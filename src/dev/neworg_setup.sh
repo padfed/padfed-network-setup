eabbd4f2f204d96717fd39c5f26840b763ff0324
fabianvarisco <fabianvarisco@gmail.com>
2020-03-27 22:03:08 -0300

    it uses lib.sh hardcode tribfed.gob.ar removing

--- src/dev/neworg_setup.sh
+++ src/dev/neworg_setup.sh
@@ -2,47 +2,24 @@
 [[ -n $DEBUG ]] && set -x
 set -Eeuo pipefail
 
-docker() {
-   [[ -v DOCKERDEBUG ]] && echo docker "$@"
-   command docker "$@"
-}
+readonly BASE="$(dirname "$(readlink -f "$0")")"
+[[ ! -v ENVIRONMENT ]] && source "$BASE/.env"
+PATH="$(realpath "$BASE/../bin"):$PATH"
+PATH="$(realpath "$BASE/../common"):$PATH"
 
-red_echo()   { echo -e "\033[1;31m$@\033[0m"; }
+. "$BASE/../common/lib.sh"
 
-green_echo() { echo -e "\033[1;32m$@\033[0m"; }
+echo_running
 
-check_param_file() {
-[[ -f "$2" ]] || { red_echo "ERROR: p$1: File [$2] not found !!!"; usage; }
-}
-
-check_file() {
-[[ -s "$1" ]] || { red_echo "ERROR: File [$1] not found !!!"; exit 1; }
-}
-
-check_dir() {
-[[ -d "$1" ]] || { red_echo "ERROR: Dir [$1] not found !!!"; exit 1; }
-}
-
-function check_exe() {
-local command="$(command -v "$1")"
-[[ -x $command ]] && { echo "exe checked [$command]"; } || { red_echo "ERROR: Exe ["$1"] not found !!!"; exit 1; } 
-}
-
-if [[ "$#" -ne 1 ]]; then
-  echo "Usage: $0 neworg_name (ex: AGIP)"
+if [[ $# -ne 1 ]]; then
+  echo_red "ERROR: $# is an invalid number of args"
+  echo "Usage: $0 neworg_name"
   echo "The script will add a neworg into the channel"
   exit 1
 fi
 
-THIS="$0"
-
-NEWORG="${1^^}" 
-DOMAIN="${NEWORG,,}".tribfed.gob.ar
-
-readonly BASE=$(dirname $(readlink -f $0))
-[[ ! -v ENVIRONMENT ]] && source $BASE/.env
-PATH=$(realpath $BASE/../bin):$PATH
-PATH=$(realpath $BASE/../common):$PATH
+NEWORG="${1^^}"
+readonly NEWORG_DOMAIN="${NEWORG,,}.$NETWORK_DOMAIN"
 
 check_exe cryptogen
 check_exe jq
@@ -57,13 +34,13 @@ make_crypto_config_yaml() {
 cat <<< "Organizations:
 PeerOrgs:
   - Name: ${NEWORG}
-    Domain: cba.tribfed.gob.ar
+    Domain: ${NEWORG_DOMAIN}
     CA:
       Hostname: ca
       Country: AR
       Province: xx
       Locality: xx
-      OrganizationalUnit: ${NEWORG}
+      OrganizationalUnit: ${NEWORG_DOMAIN}
     EnableNodeOUs: true
     Template:
       Count: 2
@@ -72,14 +49,14 @@ PeerOrgs:
 " > "$OUTPUT_CRYPTO_CONFIG_YAML"
 }
 
-# construye un configtx.yaml para que sea utilizado por 
+# construye un configtx.yaml para que sea utilizado por
 make_configtx_yaml() {
 
    local OUTPUT_CONFIGTX_YAML="$1"
 
    local MSP_DIR="." # indica que el configtx.yaml esta en el mismo directorio msp
-   local ANCHOR_PEER_NAME="peer0.${NEWORG,,}.$DOMAIN" 
-   local ANCHOR_PEER_PORT="${PEER_PORT:-7051}"
+   local ANCHOR_PEER_NAME="peer0.$NEWORG_DOMAIN"
+   local ANCHOR_PEER_PORT="$PEER_PORT"
 
    ch.make.configtx.yaml.sh \
         "$NEWORG" \
@@ -91,10 +68,9 @@ make_configtx_yaml() {
    check_file "$OUTPUT_CONFIGTX_YAML"
 }
 
-echo "########################################"
-echo "Running: $THIS"
-echo "- neworg (from p1): $NEWORG"
-echo "- channel (from .env): $CHANNEL_NAME"
+echo "NEWORG (from p1) [$NEWORG]"
+echo "CHANNEL_NAME [$CHANNEL_NAME]"
+echo "PEER_PORT [${PEER_PORT:=7051}]"
 
 readonly TMP_PATH="${BASE}/${NEWORG}_setup_tmp"
 sudo rm -Rf "$TMP_PATH"
@@ -106,17 +82,17 @@ make_crypto_config_yaml "$TMP_PATH/crypto-config.yaml"
 
 cryptogen generate --config="$TMP_PATH/crypto-config.yaml" --output="$TMP_PATH/crypto-config"
 
-readonly CRYPTOGEN_OUTPUT_ORG_DIR="$TMP_PATH/crypto-config/peerOrganizations/${DOMAIN}"
+readonly CRYPTOGEN_OUTPUT_ORG_DIR="$TMP_PATH/crypto-config/peerOrganizations/$NEWORG_DOMAIN"
 
-check_dir  "$CRYPTOGEN_OUTPUT_ORG_DIR" 
-check_dir  "$CRYPTOGEN_OUTPUT_ORG_DIR/msp" 
-check_dir  "$CRYPTOGEN_OUTPUT_ORG_DIR/tlsca" 
+check_dir "$CRYPTOGEN_OUTPUT_ORG_DIR"
+check_dir "$CRYPTOGEN_OUTPUT_ORG_DIR/msp"
+check_dir "$CRYPTOGEN_OUTPUT_ORG_DIR/tlsca"
 
 make_configtx_yaml "$CRYPTOGEN_OUTPUT_ORG_DIR/msp/configtx.yaml"
 
 echo "Coping crypto material from tmp to target ..."
 
-readonly NEWORG_TARGET_PATH="$FABRIC_INSTANCE_PATH/crypto-config/peerOrganizations/$DOMAIN"
+readonly NEWORG_TARGET_PATH="$FABRIC_INSTANCE_PATH/crypto-config/peerOrganizations/$NEWORG_DOMAIN"
 
 sudo rm -Rf "$NEWORG_TARGET_PATH"
 
@@ -128,13 +104,13 @@ sudo cp "$CRYPTOGEN_OUTPUT_ORG_DIR/tlsca/"*-cert.pem "$FABRIC_INSTANCE_PATH/peer
 
 echo "Making update_tx ..."
 
-readonly UPDATE_IN_ENVELOPE_PB="$TMP_PATH"/${NEWORG}_update_in_envelope.pb
+readonly UPDATE_IN_ENVELOPE_PB="$TMP_PATH/${NEWORG}_update_in_envelope.pb"
 
 ch.config.tool.sh add \
-         -m $NEWORG \
+         -m "$NEWORG" \
          -k org \
          -v "$CRYPTOGEN_OUTPUT_ORG_DIR/msp" \
-         -c $CHANNEL_NAME \
+         -c "$CHANNEL_NAME" \
          -g Application \
          -u "$UPDATE_IN_ENVELOPE_PB"
 
@@ -154,38 +130,38 @@ check_file "$UPDATE_IN_ENVELOPE_PB"
 # so we simply need to issue the peer channel signconfigtx command:
 
 readonly CLI=peer0_afip_cli
-readonly CLI_SIGNING_PATH="${FABRIC_INSTANCE_PATH}"/${CLI}/signing && check_dir "$CLI_SIGNING_PATH"
+readonly CLI_SIGNING_PATH="${FABRIC_INSTANCE_PATH}/${CLI}/signing" && check_dir "$CLI_SIGNING_PATH"
 
 cp "${UPDATE_IN_ENVELOPE_PB}" "${UPDATE_IN_ENVELOPE_PB}.unsigned"
-SIGNED_FILENAME=${UPDATE_IN_ENVELOPE_PB}
+SIGNED_FILENAME="$UPDATE_IN_ENVELOPE_PB"
 
-for ORG in ${ORGS_WITH_PEERS}; do
+for ORG in $ORGS_WITH_PEERS; do
 
-    if [[ "$ORG" == "AFIP" ]]; then
+    if [[ $ORG == AFIP ]]; then
        echo "Skiping AFIP because AFIP signature will be attached executing peer channel update ..."
     else
-       echo "Signing with ${CLI} and ${ORG} crypto material ..."
+       echo "Signing with $CLI and $ORG crypto material ..."
 
-       ORG_DOMAIN="${ORG,,}.tribfed.gob.ar"
-       
-       ADMIN_DIR="${FABRIC_INSTANCE_PATH}/crypto-config/peerOrganizations/${ORG_DOMAIN}/users/Admin@${ORG_DOMAIN}"
+       ORG_DOMAIN="${ORG,,}.$NETWORK_DOMAIN"
+
+       ADMIN_DIR="${FABRIC_INSTANCE_PATH}/crypto-config/peerOrganizations/${ORG_DOMAIN}/users/Admin@$ORG_DOMAIN"
        check_dir "$ADMIN_DIR/msp"
        check_dir "$ADMIN_DIR/tls"
 
-       SIGNED_FILENAME=${SIGNED_FILENAME}.signed_by_${ORG}
-       rm -f ${SIGNED_FILENAME}
-       
-       ch.sign.update.tx.sh ${CLI} \
+       SIGNED_FILENAME="${SIGNED_FILENAME}.signed_by_$ORG"
+       rm -f "$SIGNED_FILENAME"
+
+       ch.sign.update.tx.sh "$CLI" \
                       "$CLI_SIGNING_PATH" \
-                       ${ORG} \
+                      "$ORG" \
                       "$ADMIN_DIR" \
-                       peer0.${ORG_DOMAIN}:7051 \
-                       ${UPDATE_IN_ENVELOPE_PB} \
-                       ${SIGNED_FILENAME}
+                      "peer0.${ORG_DOMAIN}:$PEER_PORT" \
+                      "$UPDATE_IN_ENVELOPE_PB" \
+                      "$SIGNED_FILENAME"
 
-       check_file ${SIGNED_FILENAME}
+       check_file "$SIGNED_FILENAME"
 
-       cp ${SIGNED_FILENAME} ${UPDATE_IN_ENVELOPE_PB}
+       cp "$SIGNED_FILENAME" "$UPDATE_IN_ENVELOPE_PB"
     fi
 done
 
@@ -193,25 +169,25 @@ done
 # The Org2 Admin signature will be attached to this call
 # so there is no need to manually sign the protobuf a second time.
 
-echo "########################################"
-echo Updating channel, adding ${NEWORG} ...
+echo_sep "Updating channel, adding $NEWORG ..."
 
-if [[ ${TLS_ENABLED:="true"} == "true" ]]; then
-    PEER_CLI_TLS_PARAMETERS="--tls --cafile /etc/hyperledger/orderer/tls/tlsca.afip.tribfed.gob.ar-cert.pem"
+TLS_PARAMETERS=""
+if [[ $TLS_ENABLED == true ]]; then
+    TLS_PARAMETERS="--tls --cafile /etc/hyperledger/orderer/tls/tlsca.afip.tribfed.gob.ar-cert.pem"
 
-    if [[ {$TLS_CLIENT_AUTH_REQUIRED:="false"} == "true" ]]; then
-       PEER_CLI_TLS_PARAMETERS="${PEER_CLI_TLS_PARAMETERS} --clientauth --keyfile /etc/hyperledger/tls/client.key --certfile /etc/hyperledger/tls/client.crt"
+    if [[ $TLS_CLIENT_AUTH_REQUIRED == true ]]; then
+       TLS_PARAMETERS="$TLS_PARAMETERS --clientauth"
+       TLS_PARAMETERS="$TLS_PARAMETERS --keyfile /etc/hyperledger/tls/client.key"
+       TLS_PARAMETERS="$TLS_PARAMETERS --certfile /etc/hyperledger/tls/client.crt"
     fi
 fi
 
-docker exec $CLI rm -f /signing/*
+docker exec "$CLI" rm -f /signing/*
 
-docker cp "$SIGNED_FILENAME" $CLI:/signing
+docker cp "$SIGNED_FILENAME" "$CLI:/signing"
 
 echo "Setting TLS parameters for peer0_afip_cli ..."
 
-docker exec $CLI peer channel update -f "/signing/$( basename $SIGNED_FILENAME)" $PEER_CLI_TLS_PARAMETERS -o $ORDERER -c $CHANNEL_NAME
+docker exec "$CLI" peer channel update -f "/signing/$(basename "$SIGNED_FILENAME")" $TLS_PARAMETERS -o "$ORDERER" -c "$CHANNEL_NAME"
 
-echo "########################################"
-green_echo "[${THIS}] - New org ${NEWORG} added to ${CHANNEL_NAME}: OK !!!"
-echo "########################################"
+echo_success "Org $NEWORG added to $CHANNEL_NAME"
--- src/dev/neworg_start.sh
+++ src/dev/neworg_start.sh
@@ -2,40 +2,24 @@
 [[ -n $DEBUG ]] && set -x
 set -Eeuo pipefail
 
-readonly BASE=$(dirname $(readlink -f $0))
-[[ ! -v ENVIRONMENT ]] && source $BASE/.env
+readonly BASE="$(dirname "$(readlink -f "$0")")"
+[[ ! -v ENVIRONMENT ]] && source "$BASE/.env"
 
-readonly THIS="$0"
+. "$BASE/../common/lib.sh"
 
-PATH=$(realpath $BASE/../common):$PATH
+echo_running
 
-function check_exe() {
-   local command="$(command -v $1)"
-   [[ -x $command ]] && { echo "exe checked [$command]"; } || { echo_red "ERROR: Exe ["$1"] not found !!!"; exit 1; } 
-}
-
-function docker() {
-   [[ -v DOCKERDEBUG ]] && echo docker "$@"
-   command docker "$@"
-}
-
-function echo_red()   { echo -e "\033[1;31m$@\033[0m"; }
-
-function echo_green() { echo -e "\033[1;32m$@\033[0m"; }
-
-function check_file() {
-   [[ -f "$1" ]] || { echo_red "ERROR: File [$1] not found !!!"; exit 1; }
-}
+PATH="$(realpath "$BASE/../common"):$PATH"
 
 function usage() {
-   echo "Usage: $0 neworg_name (ex: AGIP)"
-   echo "The script will start peers of neworg"
+   echo "Usage: $0 org"
+   echo "The script starts peers of org"
    exit 1
 }
 
 function fetch() {
-   echo "$CLI peer channel fetch $1 into $2 ..."
-   docker exec $CLI peer channel fetch $1 $PEER_CLI_TLS_PARAMETERS -o $ORDERER -c $CHANNEL_NAME "$2"
+   echo_sep "$CLI peer channel fetch $1 into $2 ..."
+   docker exec "$CLI" peer channel fetch "$1" $TLS_PARAMETERS -o "$ORDERER" -c "$CHANNEL_NAME" "$2"
 }
 
 if [[ $# -ne 1 ]]; then
@@ -45,78 +29,75 @@ fi
 
 check_exe ch.config.tool.sh
 
-readonly NEWORG=${1^^}
+NEWORG="${1^^}"
 
-readonly DOCKER_FILE="${BASE}"/${NEWORG}.docker-compose.yml && check_file "${DOCKER_FILE}"
+readonly DOCKER_FILE="$BASE/${NEWORG}.docker-compose.yml" && check_file "$DOCKER_FILE"
 
-if [[ "$TLS_ENABLED" == "true" ]]; then
-    PEER_CLI_TLS_PARAMETERS="--tls --cafile /etc/hyperledger/orderer/tls/tlsca.afip.tribfed.gob.ar-cert.pem"
-
-    if [[ "$TLS_CLIENT_AUTH_REQUIRED" == "true" ]]; then
-       PEER_CLI_TLS_PARAMETERS="${PEER_CLI_TLS_PARAMETERS} --clientauth --keyfile /etc/hyperledger/tls/client.key --certfile /etc/hyperledger/tls/client.crt"
-    fi
-fi
-
-docker-compose -f "${DOCKER_FILE}" down
-docker-compose -f "${DOCKER_FILE}" up -d
+docker-compose -f "$DOCKER_FILE" down
+docker-compose -f "$DOCKER_FILE" up -d
 
 # wait for Hyperledger Fabric to start
 echo "Waiting for fabric to complete start up..."
-while ! (docker exec peer0_${NEWORG,,}_cli peer node status  | grep "STARTED")
+while ! (docker exec "peer0_${NEWORG,,}_cli" peer node status  | grep "STARTED")
 do
     echo "..."
     sleep 1
 done
 
-readonly CONFIG_BLOCK_0=${CHANNEL_NAME}_config_block_zero.block
-readonly CONFIG_BLOCK=${CHANNEL_NAME}_config_block.block
-readonly SET_ANCHOR_PEER_TX=${NEWORG}_set_anchor_peer.tx
+TLS_PARAMETERS=""
+if [[ $TLS_ENABLED == true ]]; then
+    TLS_PARAMETERS="--tls --cafile /etc/hyperledger/orderer/tls/tlsca.afip.tribfed.gob.ar-cert.pem"
+
+    if [[ $TLS_CLIENT_AUTH_REQUIRED == true ]]; then
+       TLS_PARAMETERS="$TLS_PARAMETERS --clientauth"
+       TLS_PARAMETERS="$TLS_PARAMETERS --keyfile /etc/hyperledger/tls/client.key"
+       TLS_PARAMETERS="$TLS_PARAMETERS --certfile /etc/hyperledger/tls/client.crt"
+    fi
+fi
+
+readonly CONFIG_BLOCK_0="${CHANNEL_NAME}_config_block_zero.block"
+readonly CONFIG_BLOCK="${CHANNEL_NAME}_config_block.block"
+readonly SET_ANCHOR_PEER_TX="${NEWORG}_set_anchor_peer.tx"
 readonly CLI_WORKING_DIR="/opt/gopath/src/github.com/hyperledger/fabric/peer"
 
-rm -f $CONFIG_BLOCK_0 $CONFIG_BLOCK $SET_ANCHOR_PEER_TX
+rm -f "$CONFIG_BLOCK_0" "$CONFIG_BLOCK" "$SET_ANCHOR_PEER_TX"
 
 for peer in $PEERS; do
 
-    CLI=${peer}_${NEWORG,,}_cli
+    CLI="${peer}_${NEWORG,,}_cli"
 
     # TODO: verificar si el container esta levantado
 
-    echo "####################################################"
-    fetch 0 $CONFIG_BLOCK_0
+    fetch 0 "$CONFIG_BLOCK_0"
 
-    echo "####################################################"
-    echo "$CLI peer channel join ..."
-    docker exec $CLI peer channel join $PEER_CLI_TLS_PARAMETERS -b $CONFIG_BLOCK_0
+    echo_sep "$CLI peer channel join ..."
+    docker exec "$CLI" peer channel join $TLS_PARAMETERS -b "$CONFIG_BLOCK_0"
 
-    if [[ $peer == "peer0" ]]; then
+    if [[ $peer == peer0 ]]; then
        # los peer0 se configuran como anchor
 
-       readonly ANCHOR_PEER_NAME="peer0.${NEWORG,,}.tribfed.gob.ar"
+       readonly ANCHOR_PEER_NAME="peer0.${NEWORG,,}.$NETWORK_DOMAIN"
 
-       echo "####################################################"
        fetch config "$CONFIG_BLOCK"
 
-       docker exec $CLI cat $(basename $CONFIG_BLOCK) > $CONFIG_BLOCK
+       docker exec "$CLI" cat "$(basename "$CONFIG_BLOCK")" > "$CONFIG_BLOCK"
 
        check_file "$CONFIG_BLOCK"
 
        ch.config.tool.sh set_anchor \
                          -o "$CONFIG_BLOCK" \
-                         -m $NEWORG \
-                         -n $ANCHOR_PEER_NAME \
+                         -m "$NEWORG" \
+                         -n "$ANCHOR_PEER_NAME" \
                          -u "$SET_ANCHOR_PEER_TX"
 
-       docker cp "$SET_ANCHOR_PEER_TX" $CLI:"$CLI_WORKING_DIR/$SET_ANCHOR_PEER_TX"
+       docker cp "$SET_ANCHOR_PEER_TX" "$CLI:$CLI_WORKING_DIR/$SET_ANCHOR_PEER_TX"
 
-       echo "####################################################"
-       echo "Ejecutando $CLI peer channel update add anchor ..."
-       docker exec $CLI peer channel update $PEER_CLI_TLS_PARAMETERS -o $ORDERER -c $CHANNEL_NAME \
+       echo_sep "Ejecutando $CLI peer channel update add anchor ..."
+       docker exec "$CLI" peer channel update $TLS_PARAMETERS -o "$ORDERER" -c "$CHANNEL_NAME" \
                      -f "$CLI_WORKING_DIR/$SET_ANCHOR_PEER_TX"
-       
-       echo_green "[$THIS] - Peer [$ANCHOR_PEER_NAME] configured as anchor peer of [$NEWORG] !!!"
+
+       echo_green "$ANCHOR_PEER_NAME configured as anchor peer"
     fi
 done
 
-echo "########################################"
-echo_green "[$THIS] - Peers of new org $NEWORG joined to $CHANNEL_NAME: OK !!!"
-echo "########################################"
+echo_success "$PEERS of $NEWORG joined to $CHANNEL_NAME"
