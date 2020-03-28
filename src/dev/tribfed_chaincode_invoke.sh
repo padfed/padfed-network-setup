06417c388aeb92d1966c7cd792819e09196385fe
fabianvarisco <fabianvarisco@gmail.com>
2020-03-28 10:32:15 -0300

    get zero peers from docker ps

--- src/dev/tribfed_chaincode_invoke.sh
+++ src/dev/tribfed_chaincode_invoke.sh
@@ -2,56 +2,61 @@
 [[ -n $DEBUG ]] && set -x
 set -Eeuo pipefail
 
-SUBCOMMAND="$1"
-ARGS="$2"
+readonly SUBCOMMAND="$1"
+readonly ARGS="$2"
 
-BASE=$(dirname $(readlink -f $0))
-[[ ! -v ENVIRONMENT ]] && source $BASE/.env
-
-DIR="$( dirname "${PWD}")"/dev          # BUG: dependiente del PWD / FIX: usar $BASE definido 3 líneas arriba
+readonly BASE=$(dirname "$(readlink -f "$0")")
+[[ ! -v ENVIRONMENT ]] && source "$BASE/.env"
 
 docker() {
    [[ -v DOCKERDEBUG ]] && echo docker "$@"
    command docker "$@"
 }
 
-PEER_CLI_TLS_WITH_ORG=""
-
-if [[ "$TLS_ENABLED" == "true" ]]; then
-
-  PEER_CLI_TLS_WITH_ORDERER="--tls --cafile /etc/hyperledger/orderer/tls/tlsca.afip.tribfed.gob.ar-cert.pem"
-
-  ORGS=AFIP
-  [[ $SUBCOMMAND == invoke ]] && ORGS=$ORGS_WITH_PEERS
-  for ORG in $ORGS; do
-      echo "Adding peerAddress of peer0 $ORG ..."
-      PEER_CLI_TLS_WITH_ORG="$PEER_CLI_TLS_WITH_ORG --peerAddresses peer0.${ORG,,}.tribfed.gob.ar:7051 --tlsRootCertFiles /etc/hyperledger/tls_root_cas/tlsca.${ORG,,}.tribfed.gob.ar-cert.pem"
-  done
-
-  if [[ "$TLS_CLIENT_AUTH_REQUIRED" == "true" ]]; then
-       PEER_CLI_TLS_WITH_ORDERER="$PEER_CLI_TLS_WITH_ORDERER --clientauth --keyfile /etc/hyperledger/tls/client.key --certfile /etc/hyperledger/tls/client.crt"
-  fi
-
-fi
-
-case $SUBCOMMAND in
-  query)
-    WAIT_FOR_EVENT=""
-    ;;
-  invoke)
-    WAIT_FOR_EVENT="--waitForEvent"
-    ;;
-  *)
-    echo "Usage: $0 [invoke|query] {json-args}"
-    exit 1
+case "$SUBCOMMAND" in
+query)  readonly WAIT_FOR_EVENT="" ;;
+invoke) readonly WAIT_FOR_EVENT="--waitForEvent" ;;
+*) echo "Usage: $0 [invoke|query] {json-args}"
+   exit 1
 esac
 
-docker exec peer0_afip_cli peer chaincode $SUBCOMMAND \
-         -o $ORDERER \
-         $PEER_CLI_TLS_WITH_ORDERER \
-         $PEER_CLI_TLS_WITH_ORG \
-         -C $CHANNEL_NAME \
-         -n $CHAINCODE_NAME \
-         $WAIT_FOR_EVENT \
+readonly ZERO_PEERS=$(docker ps --format \{\{.Names\}\} --filter expose="$PEER_PORT" | grep peer0)
+
+if [[ -z $ZERO_PEERS ]]; then
+  echo "no peer0 was found"
+  exit 1
+fi
+
+echo "ZERO PEERS [$ZERO_PEERS]"
+
+TLS_WITH_PEERS=""
+ORDERER_PARAMS=""
+
+if [[ $SUBCOMMAND == invoke ]]; then
+
+  for peer in $ZERO_PEERS; do
+    org_domain="${peer/#peer0.}" # remove "peer0." from $peer if "peer0." is found at the begining
+    echo "Adding peerAddress for $peer of $org_domain ..."
+    TLS_WITH_PEERS="$TLS_WITH_PEERS --peerAddresses $peer:$PEER_PORT"
+    TLS_WITH_PEERS="$TLS_WITH_PEERS --tlsRootCertFiles /etc/hyperledger/tls_root_cas/tlsca.$org_domain-cert.pem"
+  done
+
+  ORDERER_PARAMS="-o $ORDERER"
+  if [[ $TLS_ENABLED == true ]]; then
+    ORDERER_PARAMS="$ORDERER_PARAMS --tls --cafile /etc/hyperledger/orderer/tls/tlsca.afip.$NETWORK_DOMAIN-cert.pem"
+    if [[ $TLS_CLIENT_AUTH_REQUIRED == true ]]; then
+      ORDERER_PARAMS="$ORDERER_PARAMS --clientauth"
+      ORDERER_PARAMS="$ORDERER_PARAMS --keyfile /etc/hyperledger/tls/client.key"
+      ORDERER_PARAMS="$ORDERER_PARAMS --certfile /etc/hyperledger/tls/client.crt"
+    fi
+  fi
+fi
+
+docker exec peer0_afip_cli peer chaincode "$SUBCOMMAND" \
+         $ORDERER_PARAMS \
+         $TLS_WITH_PEERS \
+         -C "$CHANNEL_NAME" \
+         -n "$CHAINCODE_NAME" \
+         "$WAIT_FOR_EVENT" \
          --logging-level info \
          -c "$ARGS"
