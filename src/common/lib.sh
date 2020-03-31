333f5e91e68371ddde2e564a607c2b56c5c43d15
fabianvarisco <fabianvarisco@gmail.com>
2020-03-30 23:13:44 -0300

    - Declare and assign separately to avoid masking return values.shellcheck(SC2155) - new get_tls_parameters

--- src/common/lib.sh
+++ src/common/lib.sh
@@ -4,15 +4,15 @@ readonly BOLD=$(tput bold)
 readonly GREEN=$(tput setaf 2)
 readonly RED=$(tput setaf 1)
 readonly NORMAL=$(tput sgr0)
+readonly BLUE="\033[1;34m"
 
-function echo_red()   { echo -e "$RED$@$NORMAL"; }
-
-function echo_green() { echo -e "$GREEN$@$NORMAL"; }
-
-function echo_blue()  { echo -e "\033[1;34m$@$NORMAL"; }
+function echo_red()   { echo -e "$RED$*$NORMAL"; }
+function echo_green() { echo -e "$GREEN$*$NORMAL"; }
+function echo_blue()  { echo -e "$BLUE$*$NORMAL"; }
 
 function docker() {
-   [[ -v DOCKERDEBUG ]] && echo docker "$@"
+   [[ -v DOCKERDEBUG ]] && echo "[DOCKERDEBUG] docker $*"
+
    command docker "$@"
 }
 
@@ -21,9 +21,7 @@ function echo_sep() {
 
    for i in {1..80}; do echo -n "-"; done
    echo
-   if [[ $# -eq 1 ]]; then
-      echo $1
-   fi
+   [[ $# -eq 1 ]] && echo "$1"
 }
 
 function echo_bold_sep() {
@@ -34,34 +32,41 @@ function echo_bold_sep() {
 }
 
 function check_param_file() {
-[[ -f "$2" ]] || { echo_red "ERROR: p$1: File [$2] not found !!!"; exit 1; }
+[[ -f $2 ]] || { echo_red "ERROR: p$1: File [$2] not found !!!"; exit 1; }
 }
 
 function check_file() {
-[[ -f "$1" ]] || { echo_red "ERROR: File [$1] not found !!!"; exit 1; }
+[[ -f $1 ]] || { echo_red "ERROR: File [$1] not found !!!"; exit 1; }
 }
 
 function check_file_size() {
-[[ -r "$1" && -s $1 ]] || { echo_red "ERROR: File [$1] not readeable or empty !!!"; exit 1; }
+[[ -r $1 && -s $1 ]] || { echo_red "ERROR: File [$1] not readeable or empty !!!"; exit 1; }
 }
 
 function check_dir() {
-[[ -d "$1" ]] || { echo_red "ERROR: Dir [$1] not found !!!"; exit 1; }
+[[ -d $1 ]] || { echo_red "ERROR: Dir [$1] not found !!!"; exit 1; }
 }
 
 function check_no_empty_dir() {
 check_dir "$1"
-local FILES=$(ls -A "$1")
-[[ "$FILES" ]] || { echo_red "ERROR: Dir [$1] is empty !!!"; exit 1; }
+local FILES
+FILES=$(ls -A "$1")
+[[ $FILES ]] || { echo_red "ERROR: Dir [$1] is empty !!!"; exit 1; }
 }
 
 function check_exe() {
-local command="$(command -v $1)"
-[[ -x $command ]] && { echo "exe checked [$command]"; } || { echo_red "ERROR: Exe ["$1"] not found !!!"; exit 1; }
+local command
+command="$(command -v "$1")"
+if [[ -x $command ]]; then
+  echo "checked command [$command]"
+  return 0 # OK
+fi
+echo_red "ERROR: command [$1] not found !!!"
+exit 1 # Fail
 }
 
 function check_number_of_params() {
-if [[ "$1" -ne $2 ]]; then
+if [[ $1 -ne "$2" ]]; then
    echo_red "ERROR: $1 unexpected number of params"
    usage
 fi
@@ -80,7 +85,8 @@ if [[ -d "$SOURCE_DIR" ]]; then
 
     mkdir -p "${SOURCE_DIR}-backup"
 
-    local FILE=backup-$(basename "$SOURCE_DIR")$TAG_NAME$(date +%Y%m%d%H%M%S).tar.xz
+    local FILE
+    FILE=backup-$(basename "$SOURCE_DIR")$TAG_NAME$(date +%Y%m%d%H%M%S).tar.xz
     tar --create --gzip --file="${SOURCE_DIR}-backup/$FILE" "$SOURCE_DIR"
 fi
 }
@@ -120,8 +126,7 @@ function echo_running() {
 }
 
 function echo_success() {
-#   echo_sep
-    echo_green "[${THIS:-}] ${1:-} exit OK !!!"
+    echo_green "[${THIS:-}] ${1:-"exit OK !!!"}"
     echo_bold_sep
 }
 
@@ -130,7 +135,7 @@ function is_x509_crt() {
 }
 
 check_x509_crt() {
-is_x509_crt "$1" || { echo_red "ERROR: File ["$1"] is not a x509 certificate !!!"; exit 1; }
+is_x509_crt "$1" || { echo_red "ERROR: File [$1] is not a x509 certificate !!!"; exit 1; }
 }
 
 function is_crl() {
@@ -138,7 +143,7 @@ function is_crl() {
 }
 
 check_crl() {
-is_crl "$1" || { echo_red "ERROR: File ["$1"] is not a crl certificate !!!"; exit 1; }
+is_crl "$1" || { echo_red "ERROR: File [$1] is not a crl certificate !!!"; exit 1; }
 }
 
 function jq_check_value() {
@@ -147,9 +152,24 @@ function jq_check_value() {
    local JSON_PATH="$2"
    local WANT="$3"
 
-   local VALUE=$( jq "$JSON_PATH" "$JSON_FILE" )
+   local VALUE
+   VALUE=$( jq "$JSON_PATH" "$JSON_FILE" )
 
    echo "WANT [$WANT] VALUE [$VALUE]"
 
-   [[ $WANT == $VALUE ]]
+   [[ $WANT == "$VALUE" ]]
+}
+
+function get_tls_parameters() {
+  local TLS_PARAMETERS=""
+  if [[ $TLS_ENABLED == true ]]; then
+    TLS_PARAMETERS="--tls --cafile /etc/hyperledger/orderer/tls/tlsca.afip.tribfed.gob.ar-cert.pem"
+
+    if [[ $TLS_CLIENT_AUTH_REQUIRED == true ]]; then
+       TLS_PARAMETERS="$TLS_PARAMETERS --clientauth"
+       TLS_PARAMETERS="$TLS_PARAMETERS --keyfile /etc/hyperledger/admin/tls/client.key"
+       TLS_PARAMETERS="$TLS_PARAMETERS --certfile /etc/hyperledger/admin/tls/client.crt"
+    fi
+  fi
+  echo "$TLS_PARAMETERS"
 }
