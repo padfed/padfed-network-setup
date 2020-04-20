2a6d8936fcb9753bdfc55544dc107fda1c12c53e
fabianvarisco <fabianvarisco@gmail.com>
2020-04-20 17:54:15 -0300

    MSPID case sensitive support

--- src/prod/config/cc.install.sh
+++ src/prod/config/cc.install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 [[ -n ${DEBUG:-} ]] && set -x
 set -Eeuo pipefail
-BASE=$(dirname $(readlink -f $0))
-source $BASE/.env
+readonly BASE=$(dirname "$(readlink -f "$0")")
+source "$BASE/.env"
 
 function package() {
   local CHAINCODE_FILE_TAR_XZ="$TARGET/download/$CHAINCODE_NAME-$VERSION.tar.xz"
 
   if ! [[ -r $CHAINCODE_FILE_TAR_XZ ]]; then
-     echo "Error: no esta disponible el archivo [$(basename $CHAINCODE_FILE_TAR_XZ)] en [$TARGET/download/]" >&2
+     echo "Error: no esta disponible el archivo [$(basename "$CHAINCODE_FILE_TAR_XZ")] en [$TARGET/download/]" >&2
      exit 1
   fi
   if ! [[ -x "$(command -v tar)" ]]; then
@@ -21,17 +21,17 @@ function package() {
   mkdir -p "$PKG"
   tar -C "$PKG" -xaf "$CHAINCODE_FILE_TAR_XZ"
 
-  docker exec $CONTAINER \
+  docker exec "$CONTAINER" \
     peer chaincode package \
-      -n $CHAINCODE_NAME \
-      -p $CHAINCODE_PATH \
-      -v $VERSION \
-      /opt/gopath/deploy/${CHAINCODE_NAME}-${VERSION}.ccd
+      -n "$CHAINCODE_NAME" \
+      -p "$CHAINCODE_PATH" \
+      -v "$VERSION" \
+      "/opt/gopath/deploy/${CHAINCODE_NAME}-${VERSION}.ccd"
 }
 
 ############################################################
 
-if [[ -z "$1" ]]; then
+if [[ -z $1 ]]; then
   echo "Error: p1 debe ser la version del CC a instalar"
 	exit 1
 fi
@@ -43,18 +43,18 @@ if ! (echo "$VERSION" | grep -qE '^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-
   exit 1
 fi
 
-readonly TARGET="${FABRIC_INSTANCE_PATH}/${MSPID^^}-${NODE_BASENAME,,}/gopath"
+readonly TARGET="${FABRIC_INSTANCE_PATH}/$MSPID-${NODE_BASENAME,,}/gopath"
 
 mkdir -p "$TARGET/deploy"
 
 readonly CONTAINER="${NODE_NAME}.cli"
 
 if [[ ! -f "$TARGET/deploy/${CHAINCODE_NAME}-${VERSION}.ccd" ]]; then
-   package 
+   package
 fi
 
-if ! (docker exec $CONTAINER peer chaincode list -C $CHANNEL_NAME --installed | sed -E "/Name: $CHAINCODE_NAME,/s/.* Version: ([^,]+),.*/\1/" | grep -q ${VERSION}); then
-  docker exec $CONTAINER \
+if ! (docker exec "$CONTAINER" peer chaincode list -C "$CHANNEL_NAME" --installed | sed -E "/Name: $CHAINCODE_NAME,/s/.* Version: ([^,]+),.*/\1/" | grep -q ${VERSION}); then
+  docker exec "$CONTAINER" \
     peer chaincode install \
-      /opt/gopath/deploy/${CHAINCODE_NAME}-${VERSION}.ccd
+      "/opt/gopath/deploy/${CHAINCODE_NAME}-${VERSION}.ccd"
 fi
